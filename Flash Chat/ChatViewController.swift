fb883afd04fe39812b4686104b25cbedd130214d
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-11-08 22:35:35 +0100

    finished App

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Flash Chat
 //
-//  Created by Angela Yu on 29/08/2015.
+//  Created by Angela Yu on 29/08/2015. Modified by FG
 //  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
